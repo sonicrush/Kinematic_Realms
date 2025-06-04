5e954c22add938f57753269858cd9381ea0e2a83
Saneec <crisman2005@gmail.com>
2025-06-04 17:48:52 -0400

    Improved accelerationTracker.cs
    Made the logic update every 0.1 seconds, which allows for accurate calculation of acceleration from the change in velocity.
    Also my first time using a coroutine!

--- Kinematic Realms/Assets/Scripts/accelerationTracker.cs
+++ Kinematic Realms/Assets/Scripts/accelerationTracker.cs
@@ -1,31 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
-
-public class NewMonoBehaviourScript : MonoBehaviour
+public class AccelerationExtrapolation : MonoBehaviour
 {
     public GameObject targetObject;
     private Vector3 _previousVelocity;
     private Vector3 _presentVelocity;
     private Rigidbody _rigidbody;
-    private double[] _accelerationCurrent = new double[3];
+    [System.NonSerialized]public double accelerationX;
+    [System.NonSerialized]public double accelerationY;
+    [System.NonSerialized]public double accelerationZ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+	IEnumerator perSecond() 
+	{
+		while(true) 
+		{
+		_previousVelocity = _presentVelocity;
+        _presentVelocity = _rigidbody.GetPointVelocity(transform.TransformPoint(targetObject.transform.position));
+        accelerationX = (_presentVelocity[0] - _previousVelocity[0]) / 0.1;
+        accelerationY = (_presentVelocity[1] - _previousVelocity[1]) / 0.1;
+        accelerationZ = (_presentVelocity[2] - _previousVelocity[2]) / 0.1;
+		yield return new WaitForSeconds(0.1f);
+		}
+	}
     void Start()
     { 
         _rigidbody = gameObject.GetComponent<Rigidbody>();
         _presentVelocity = _rigidbody.GetPointVelocity(transform.TransformPoint(targetObject.transform.position));
-        ;
+        StartCoroutine(perSecond());
     }
 
     // Update is called once per frame
     void Update()
     {
-        _previousVelocity = _presentVelocity;
-        _presentVelocity = _rigidbody.GetPointVelocity(transform.TransformPoint(targetObject.transform.position));
-        _accelerationCurrent[0] = _presentVelocity[0] - _previousVelocity[0];
-        _accelerationCurrent[1] = _presentVelocity[1] - _previousVelocity[1];
-        _accelerationCurrent[2] = _presentVelocity[2] - _previousVelocity[2];
-        print("X velocity is : " + _accelerationCurrent[0]);
-        print("Y velocity is : " + _accelerationCurrent[1]);
-        print("Z velocity is : " + _accelerationCurrent[2]);
+
     }
 }
