87912a7cc275e389eb7c6ee0878402cdcb8209b8
Saneec <crisman2005@gmail.com>
2025-06-04 23:38:53 -0400

    Oh my god.
    I don't even want to talk about it. Do not ask me about accelerationTracker.cs in my life. Don't.
    
    For the actual important changes, I reorganized the file structure and also added a test script that allows the developer to move the ball with WASD and Space. It includes a global Input System as well.

---
+++ Kinematic Realms/Assets/Scripts/TestScripts/Movement Test.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementTest : MonoBehaviour
+{
+    private new Rigidbody _rigidbody;
+    private InputAction _up;
+    private InputAction _down;
+    private InputAction _left;
+    private InputAction _right;
+    private InputAction _jump;
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _up = InputSystem.actions.FindAction("Player/Forward");
+        _down = InputSystem.actions.FindAction("Player/Backward");
+        _left = InputSystem.actions.FindAction("Player/Left");
+        _right = InputSystem.actions.FindAction("Player/Right");
+        _jump = InputSystem.actions.FindAction("Player/Up");
+    }
+    void FixedUpdate()
+    {
+        if(_up.IsPressed())
+            _rigidbody.AddForce(Vector3.forward * 2f, ForceMode.Force);
+        if(_left.IsPressed())
+            _rigidbody.AddForce(Vector3.left * 2f, ForceMode.Force);
+        if(_down.IsPressed())
+            _rigidbody.AddForce(Vector3.back * 2f, ForceMode.Force);
+        if(_right.IsPressed())
+            _rigidbody.AddForce(Vector3.right * 2f, ForceMode.Force);
+        if(_jump.IsPressed())
+            _rigidbody.AddForce(Vector3.up * 10.8f, ForceMode.Force);
+    }
+}
