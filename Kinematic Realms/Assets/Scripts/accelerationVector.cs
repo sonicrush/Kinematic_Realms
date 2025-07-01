89622a13170e127fe9cef650a981694f6362bc57
Saneec <crisman2005@gmail.com>
2025-06-30 20:57:46 -0400

    Premature commit

--- Kinematic Realms/Assets/Scripts/accelerationVector.cs
+++ Kinematic Realms/Assets/Scripts/accelerationVector.cs
@@ -7,20 +7,42 @@ public class accelerationVector : MonoBehaviour
     private GameObject _vectorArrowAsset;
     private GameObject _vectorArrow;
     private Transform _vectorArrowTransformComponent;
-
+    public int xOffset;
+    public int yOffset;
+    public int quaterionX;
+    public int quaterionY;
+    public int quaterionZ;
+    public int quaterionW;
+    Quaternion quaterionThing;
+    Vector3 normalizedVector;
+    Vector3 vector3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _accelerationExtraporlator = gameObject.GetComponent<AccelerationExtrapolation>();
         _vectorArrowAsset = Resources.Load<GameObject>("Prefabs/vectorArrow");
-        _vectorArrow = Instantiate(_vectorArrowAsset);
+        Vector3 vector3 = new Vector3(xOffset, yOffset, 0);
+        quaterionThing = new Quaternion(quaterionX, quaterionY, quaterionZ, quaterionW);
+        _vectorArrow = Instantiate(_vectorArrowAsset, vector3,quaterionThing, gameObject.GetComponent<Transform>());
         _vectorArrowTransformComponent = _vectorArrow.GetComponent<Transform>();
-        _vectorArrowTransformComponent.SetParent(gameObject.GetComponent<Transform>(), false);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+
+        quaterionThing.w = quaterionW;
+        vector3.x = xOffset;
+        vector3.y = yOffset;
+        normalizedVector = _accelerationExtraporlator.AccelerationVector.normalized;
+        quaterionThing.x = normalizedVector.x;
+        quaterionThing.y = normalizedVector.y;
+        quaterionThing.z = normalizedVector.z;
+
+        _vectorArrowTransformComponent.Rotate(vector3);
+
+
+
     }
 }
