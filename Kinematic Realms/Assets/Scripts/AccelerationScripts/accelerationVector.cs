56ea815f87ef171da81d6909224e8ab8575b7f50
Saneec <crisman2005@gmail.com>
2025-07-22 14:07:12 -0400

    I cannot bro

--- Kinematic Realms/Assets/Scripts/AccelerationScripts/accelerationVector.cs
+++ Kinematic Realms/Assets/Scripts/AccelerationScripts/accelerationVector.cs
@@ -24,6 +24,7 @@ public class AccelerationVector : MonoBehaviour
         vectorArrow.GetComponent<ParentConstraint>().AddSource(new ConstraintSource { sourceTransform = vectorArrow.GetComponent<Transform>(), weight = 1 });
         vectorArrow.GetComponent<ParentConstraint>().SetTranslationOffset(1, new Vector3(xOffset, yOffset, zOffset));
         vectorArrowScriptComponent = vectorArrow.GetComponent<vectorArrowObject>();
+        
 
     }
 
@@ -32,13 +33,19 @@ public class AccelerationVector : MonoBehaviour
     {
         accelerationVector = _accelerationExtraporlator.AccelerationVector;
         Vector3 accelerationNormalized = accelerationVector.normalized;
-        // print("Acceleration Vector : " + accelerationVector);
-        // print("Acceleration normalized : " + accelerationNormalized);
-        // float xyAngle = Mathf.Atan2(accelerationNormalized.y, accelerationNormalized.x) * Mathf.Rad2Deg;
-        // float xyMagnitude = accelerationNormalized.x / Mathf.Cos(xyAngle);
-        // float zAngle = Mathf.Atan2(xyMagnitude, accelerationNormalized.z) * Mathf.Rad2Deg;
-        // float zMagnitude = accelerationNormalized.z / Mathf.Cos(zAngle);
-        _vectorArrowTransformComponent.rotation = Quaternion.LookRotation(accelerationNormalized);
+        float xyAngle = Mathf.Atan2(accelerationNormalized.y, accelerationNormalized.x) * Mathf.Rad2Deg;
+        float xyMagnitude = accelerationNormalized.y / Mathf.Cos(xyAngle * Mathf.Deg2Rad);
+
+        float finalAngle = Mathf.Atan2(accelerationNormalized.z, xyMagnitude) * Mathf.Rad2Deg;
+        // float totalMagnitude = accelerationNormalized.z / Mathf.Cos(zAngle);
+        // vectorArrowScriptComponent.stemLength = Mathf.Abs(totalMagnitude);
+
+
+        Vector3 xyNormalized = new Vector3(accelerationVector.x, accelerationVector.y, 0).normalized;
+        Quaternion rotation1 = Quaternion.Euler(0, 0, xyAngle);
+        Quaternion rotation2 = new Quaternion(xyNormalized.x * Mathf.Sin(finalAngle * Mathf.Deg2Rad/2), xyNormalized.y * Mathf.Sin(finalAngle* Mathf.Deg2Rad/2), xyNormalized.z * Mathf.Sin(finalAngle* Mathf.Deg2Rad/2), Mathf.Cos(finalAngle* Mathf.Deg2Rad/2));
+        Quaternion rotation3 = rotation2 * rotation1;
+        _vectorArrowTransformComponent.rotation = rotation3;
     
 
     }
